d67d46074d4f848df16b74aba6ae5329d5e755be
vviivviaan <viviangomess2002@gmail.com>
2025-05-05 16:35:39 -0300

    tela home

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,11 +1,7 @@
 import styles from './home.module.css';
-import background from '../images/img1.jpg';
 import { useNavigate } from 'react-router-dom';
-import logo_pmv from '../images/Logo_PMV_nova.png';
-import logo_praca from '../images/logo_original.png';
-
-
-// Imagens específicas de cada card
+import Header from '../../components/Header.jsx';
+import Footer from '../../components/Footer.jsx';
 import visita_individual from '../images/visita_individual.jpg';
 import visita_guiada from '../images/visita_guiada.jpg';
 import visita_institucional from '../images/visita_institucional.jpg';
@@ -15,21 +11,15 @@ export default function Home() {
 
   return (
     <>
-        <div id={styles.background} style={{ backgroundImage: `url(${background})` }}>
-          <div>
-            <img src={logo_praca} alt="Logo" className={styles.logo_home} />
-            <a href="https://www.vitoria.es.gov.br/" target ="_blank">
-              <img src={logo_pmv} alt="Logo Prefeitura" className={styles.logo_home}/>
-            </a>
-          </div>
-
+    <Header/>
+        <div id={styles.background}>
         <div className={styles.box}>
 
           {/* Card 1 - Visita Individual */}
-          <div className={styles.left}>
-            <img src={visita_individual} alt="Visita Individual" className={styles.card_image} />
+          <div className={styles.left}><img src={visita_individual} alt="Visita Individual" className={styles.card_image} />
             <h1 id={styles.left_box}>Visita Individual</h1>
-            <h2 id={styles.center_box}> </h2>
+            <h2 id={styles.left_box}>      </h2>
+            <h2 id={styles.left_box}>      </h2>
             <button className={styles.button} onClick={() => navigate('/weekcalendar')}>
               AGENDAR
             </button>
@@ -48,14 +38,13 @@ export default function Home() {
           <div className={styles.right}> <img src={visita_institucional} alt="Visita Institucional" className={styles.card_image} />
             <h1 id={styles.right_box}>Visita Institucional</h1>
             <h2 id={styles.right_box}>Escolas e outros grupos</h2>
-            <button 
-              className={styles.button} onClick={() => navigate('/weekcalendar')}>
+            <button className={styles.button} onClick={() => navigate('/weekcalendar')}>
               AGENDAR
             </button>
           </div>
-
         </div>
       </div>
+    <Footer/>
     </>
   );
 }
